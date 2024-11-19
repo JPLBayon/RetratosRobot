4a3c040d10e6d4c887c6ccbf22b4652ae20c022b
Julio Pedro López Bayón <juliolb@educastur.org>
2024-11-19 17:04:48 +0100

    Actualización

--- src/fp/dam/java/retratosrobot/Main.java
+++ src/fp/dam/java/retratosrobot/Main.java
@@ -8,6 +8,14 @@ public class Main {
 
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	
+	static String [][] opciones = {
+			{"el pelo", "WWWWWWWWW", "\\\\\\//////", "\"|||||||\"", "|||||||||"},
+			{"los ojos", "|  o o  |", "| (· ·) |", "| (O O) |", "| \\   / |"},
+			{"la nariz", "@   J   @", "{   \"   }", "[   j   ]", "<   -   >"},
+			{"la boca", "|  ===  |", "|   -   |", "|  ___  |", "|  ---  |"},
+			{"la barbilla", "\\_______/", "\\,,,,,,,/"}
+	};
+	
 	public static void main(String[] args) throws IOException {
 		System.out.print("Crear retrato Robot (s/n) : ");
 		String respuesta = in.readLine();
@@ -19,38 +27,33 @@ public class Main {
 	}
 	
 	public static void crearRetrato() throws IOException {
-		String pelo = elegirOpcion("el pelo", "WWWWWWWWW", "\\\\\\//////", "\"|||||||\"", "|||||||||");
-		String ojos = elegirOpcion("los ojos", "|  o o  |", "| (· ·) |", "| (O O) |", "| \\   / |");
-		String nariz = elegirOpcion("la nariz", "@   J   @", "{   \"   }", "[   j   ]", "<   -   >");
-		String boca = elegirOpcion("la boca", "|  ===  |", "|   -   |", "|  ___  |", "|  ---  |");
-		String barbilla = elegirOpcion("la barbilla", "\\_______/", "\\,,,,,,,/", "\\......./", "\\mmmmmmm/");
-		System.out.println(pelo);
-		System.out.println(ojos);
-		System.out.println(nariz);
-		System.out.println(boca);
-		System.out.println(barbilla);
+		String [] retrato = new String[opciones.length];
+		for (int i=0; i<opciones.length; i++)
+			retrato[i] = elegirOpcion(i);
+		for (int i=0; i<opciones.length; i++)
+			System.out.println(retrato[i]);
 	}
 	
-	public static String elegirOpcion(String rasgo, String op1, String op2, String op3, String op4) throws IOException {
-		String resultado = null;
-		int op;
-		do {
-			System.out.println("Elige una opción para " + rasgo);
-			System.out.println("1 - " + op1);
-			System.out.println("2 - " + op2);
-			System.out.println("3 - " + op3);
-			System.out.println("4 - " + op4);
-			System.out.print("Opcion (1 a 4): ");
-			op = Integer.parseInt(in.readLine());
-			switch (op) {
-			case 1: resultado =  op1; break;
-			case 2: resultado =  op2; break;
-			case 3: resultado =  op3; break;
-			case 4: resultado =  op4; break;
-			default: System.out.println("opcion incorrecta, inténtalo de nuevo");
-			}
-		} while (op < 1 || op > 4);
-		return resultado;
+	public static String elegirOpcion(int i) throws IOException {
+//		String resultado = null;
+//		int op;
+//		do {
+//			System.out.println("Elige una opción para " + rasgo);
+//			System.out.println("1 - " + op1);
+//			System.out.println("2 - " + op2);
+//			System.out.println("3 - " + op3);
+//			System.out.println("4 - " + op4);
+//			System.out.print("Opcion (1 a 4): ");
+//			op = Integer.parseInt(in.readLine());
+//			switch (op) {
+//			case 1: resultado =  op1; break;
+//			case 2: resultado =  op2; break;
+//			case 3: resultado =  op3; break;
+//			case 4: resultado =  op4; break;
+//			default: System.out.println("opcion incorrecta, inténtalo de nuevo");
+//			}
+//		} while (op < 1 || op > 4);
+//		return resultado;
 	}
 
 }
